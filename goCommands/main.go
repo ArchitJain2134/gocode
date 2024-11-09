3ed408ff8a2e42ea744e50c593cfbf37b247b0b3
ArchitJain2134 <144250786+ArchitJain2134@users.noreply.github.com>
2024-11-09 22:21:54 +0530

    basic commands

---
+++ goCommands/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Testing basic go commands")
+	// ~ go build command helps to create the executable file of our code. It creates a binary executable file
+	// but the problem is that we need to give the proper address of the file to access it
+	// ~ go install also do the same work as go build but the executable file will be stored in bin directory so 
+	// that it can be excessed from anywhere
+}
